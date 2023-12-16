8aa88b8a78cdb1f1a3f65bee44ddfe9be331697a
Patricia Frogoso <pfrogoso@gmail.com>
2023-12-16 17:28:18 -0600

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+
+  return tutorials.map(titleCase);
 }
+function titleCase(name) {
+  return name.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+const result = titleCased();
+
+console.log(result);
